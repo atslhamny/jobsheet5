722ba6a198e1388c3e923cbe5204aef63a05e18b
atslhamny <atsilahamanyph@gmail.com>
2023-10-01 20:09:21 +0700

    ini udah dimodifikasi jadi ternary

--- PemilihanPercobaan1_04.java
+++ PemilihanPercobaan1_04.java
@@ -4,13 +4,22 @@ public class PemilihanPercobaan1_04 {
     public static void main(String[] args) {
         Scanner input04 = new Scanner(System.in);
 
-        System.out.println("Masukkan Angka ");
+        System.out.println("Masukkan Angka");
         int angka = input04.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka Genap");
-        } else {
-            System.out.println("Angka Ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "ANGKA GENAP" : "ANGKA GANJIL";
+        System.out.println(hasil);
+    }
+
+    public void exp1nomod(){
+
+        // System.out.println("Masukkan Angka ");
+        // int angka = input04.nextInt();
+
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka Genap");
+        // } else {
+        //     System.out.println("Angka Ganjil");
+        // }
     }
 }
